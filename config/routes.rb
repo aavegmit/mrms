1e700f69675b196802e7641a7e64578619f75c44
Aaveg Mittal <amittal@zynga.com>
2014-05-05 01:34:05 -0700

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Mrms::Application.routes.draw do
   resources :vaccines
 
+
   resources :patients do
      collection do
 	post 'vaccinate'
@@ -11,7 +12,7 @@ Mrms::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
